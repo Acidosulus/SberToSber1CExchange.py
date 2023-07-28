de8ddbda1c3198468f628f39a1b911903cc1620a
Николай Михайлович Казаковцев <75414235+Acidosulus@users.noreply.github.com>
2023-07-28 13:31:09 +0700

    Add files via upload

--- SberToSber1CExchange.py
+++ SberToSber1CExchange.py
@@ -4,7 +4,7 @@ import pprint
 import logging
 from  stek import *
 from click import echo, style  
-
+import pandas
 
 logging.basicConfig(level=logging.INFO, filename="convert.log",filemode="a", format="%(asctime)s %(levelname)s %(message)s")
 
@@ -49,7 +49,7 @@ class SberFile():
 		self.data = self.LoadSberFile()
 		self.SaveSber1CExchange()
 		self.pay_order_number = ''
-
+		self.lastrow = ''
 
 	def LoadSberFile(self) -> list:
 		result = []
@@ -58,6 +58,7 @@ class SberFile():
 		for counter, line in enumerate(lines):
 			if line[1] == '=' or line[0] == '+':
 					self.pay_order_number = sx(line,';',';',4)
+					self.lastrow = line
 					logging.info(f'Pay order number for all is:{self.pay_order_number}')
 					break
 			llines = line.split(';')
@@ -70,9 +71,9 @@ class SberFile():
 							'fio'									:llines[6].strip(),
 							'address'								:llines[7].strip(),
 							'period'								:llines[8].strip(),
-							'transaction'							:llines[9].strip(),
-							'total'									:llines[10].strip(),
-							'comission'								:llines[11].strip(),
+							'transaction'							:llines[9].strip().replace(',','.'),
+							'total'									:llines[10].strip().replace(',','.'),
+							'comission'								:llines[11].strip().replace(',','.'),
 							})
 		
 		try:
@@ -106,12 +107,25 @@ class SberFile():
 		path = self.path + '_1CExchange.txt'
 		logging.info(f"Saving data to file:{path}")
 		result = self.header_pattern+'\n'
+		ll_array = []
 		for counter, row in enumerate(self.data):
 			agrement =get_details_from_STEK_by_agreement_number(self.agreements_data, row['nc'])
+			ll_array.append([	row['date'],
+		    					row['time'],
+								row['department'],
+								row['cacher'],
+								row['suip'],
+								row['nc'],
+								row['fio'],
+								row['address'],
+								row['period'],
+								float(row['transaction']),
+								float(row['total']),
+								float(row['comission'])])
 			result += f"""СекцияДокумент=Платежное поручение
 Номер={self.pay_order_number}_{counter}
 Дата={row['date'].replace('-','.')}
-Сумма={row['total'].replace(',','.')}
+Сумма={row['total']}
 ПлательщикСчет=
 ДатаСписано=
 Плательщик={agrement['name']}
@@ -144,10 +158,33 @@ class SberFile():
 Очередность=5
 НазначениеПлатежа={row['nc']} / {row['fio']} / {row['address']} / {row['period']}
 КонецДокумента\n"""
+
 		result += 'КонецФайла'
 		print(path)
 		str_to_file(path, result)
 		logging.info(f'Rows was saved into file:{path}')
+		ll_array.append([self.lastrow,'','','','','','','','','','',''])
+		ll_columns = [	'Дата платежа',
+						'Время платежа',
+						'Номер отделения',
+						'Номер кассира/УС/СБОЛ',
+						'СУИП',
+						'Номер договора',
+						'ФИО',
+						'Адрес',
+						'Период',
+						'Сумма операции',
+						'Сумма перевода',
+						'Сумма комисии банку']
+		df = pandas.DataFrame(ll_array, columns=ll_columns )
+		writer = pandas.ExcelWriter(f'{path}.xlsx', engine='xlsxwriter')
+		df.to_excel(writer, sheet_name='Sheet1', startrow=0)
+		sheet = writer.sheets['Sheet1']
+		sheet.autofilter(0,0,0,25)
+		sheet.set_column('A:Z', 15)
+		writer.save()
+		logging.info(f'Saved Excel file to {path}.xls')
+
 
 sf = SberFile(sys.argv[1], sys.argv[2])
 #prnt(sf.SaveSber1CExchange())
